a6d534d6362df99ff35cd65ea6da0b741584465e
Andreas Grevle <39051982+andreasgrevl@users.noreply.github.com>
2020-05-06 14:17:02 +0200

    Initial commit

---
+++ tampen57_19.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
+
+data = pd.read_csv("Tampen.txt",delimiter='\\t')
+data.to_csv('Tampen1.csv')
+
+
+df = data[['YEAR', 'TOTALHS','TOTALTP']]
+
+x = df['TOTALHS']
+
+y = df['TOTALTP']
+
+z = df['YEAR']
+
+
+sns.set(style="darkgrid", rc={'figure.figsize':(11,8)})
+sns.relplot(x="YEAR", y="TOTALHS",  kind="line", data=df);
+
+tampen57_19()
